a4d9c690b8c939b72d2925cf87c7bb6c44496557
John Mark Adriano <j.adriano@gmail.com>
2016-02-22 15:30:28 +0400

    AnimFiles
    Added idle Animation for enemies and player, also added shots

---
+++ TestRep/Assets/Scripts/CharacterClass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterClass : MonoBehaviour {
+
+	int health;
+	bool isDead;
+
+	public CharacterClass()
+	{
+		health = 10;
+		isDead = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
