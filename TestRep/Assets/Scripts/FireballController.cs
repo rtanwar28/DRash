767a38d010ce52706dbf2bd224ba7ae3330a8c9b
rtanwar28 <r.tanwar@saedubai.com>
2016-03-07 15:16:34 +0400

    Player movement
    Player movement and shooting projectiles

---
+++ TestRep/Assets/Scripts/FireballController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballController : MonoBehaviour {
+
+	public float fireSpeed; // speed for the fireball
+	Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+	
+		//Rigidbody 2D component for player fireball
+		rb2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//rb2D.velocity = new Vector2 (fireSpeed, rb2D.velocity.x);
+		rb2D.AddForce (Vector2.up * fireSpeed);
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.name == "Block") {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ TestRep/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed; // Setting the player speed
+	
+	Animator animator;
+
+	public GameObject fireball;
+	public Transform firePoint;
+	
+	// Use this for initialization
+	void Start () {
+	
+		animator = GetComponent<Animator> ();
+	}
+
+	void Update()
+	{
+		Vector2 temp = transform.position;
+		//Clamping the player to the x-axis
+		temp.x = Mathf.Clamp (transform.position.x, -4.5f, 4.5f);
+
+		//Clamping the player to the y-axis
+		temp.y = Mathf.Clamp (transform.position.y, -3.3f, 1.0f);
+
+		// Setting the player position to the clamped values along the x and the y axis.
+		transform.position = temp;
+	}
+	
+
+	void FixedUpdate()
+	{
+
+		//Left Player Movement
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			transform.Translate (Vector2.left * speed * Time.deltaTime);
+
+			animator.SetBool("mLeft",true);
+			animator.SetBool("mRight",false);
+			animator.SetBool("mDown",false);
+			animator.SetBool("mUp",false);
+			animator.SetBool("mIdle",false);
+		}
+
+		else
+		//Right Player Movement
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			transform.Translate (Vector2.right * speed * Time.deltaTime);
+
+			animator.SetBool("mRight",true);
+			animator.SetBool("mLeft",false);
+			animator.SetBool("mDown",false);
+			animator.SetBool("mUp",false);
+			animator.SetBool("mIdle",false);
+		}
+
+		else
+		//Up Player Movement
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			transform.Translate (Vector2.up * speed * Time.deltaTime);
+
+			animator.SetBool("mUp",true);
+			animator.SetBool("mLeft",false);
+			animator.SetBool("mRight",false);
+			animator.SetBool("mDown",false);
+			animator.SetBool("mIdle",false);
+
+		}
+
+		else
+		//Down Player Movement
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			transform.Translate (Vector2.down * speed * Time.deltaTime);
+
+			animator.SetBool("mDown",true);
+			animator.SetBool("mUp",false);
+			animator.SetBool("mLeft",false);
+			animator.SetBool("mRight",false);
+			animator.SetBool("mIdle",false);
+		}
+
+		//Idle State
+		else 
+		{
+			animator.SetBool("mIdle",true);
+			animator.SetBool("mLeft",false);
+			animator.SetBool("mRight",false);
+			animator.SetBool("mDown",false);
+			animator.SetBool("mUp",false);
+		}
+
+		//Instantiating player fireballs
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			Instantiate(fireball,firePoint.position,firePoint.rotation);
+		}
+
+	}
+}
