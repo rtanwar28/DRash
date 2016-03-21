e79e28fd9e4a86921e417faa80a63591fe4e6abb
rtanwar28 <r.tanwar@saedubai.com>
2016-03-21 10:34:58 +0400

    Track Player Scores
    Scripts to : 
    1. Add score to the text file
    2. Get player info from input field
    3. Tracking the scores

---
+++ TestRep/PlayerInfo.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class PlayerInfo : MonoBehaviour {
+
+	public InputField iField; // variable for input field
+	string pName;
+
+	TrackScore trackObj;
+
+	List<string> nameList = new List<string>();
+	List<int> scoreList=new List<int>();
+
+	// Use this for initialization
+	void Start () {
+
+		trackObj = GetComponent<TrackScore> ();
+
+		//ReadPlayerName ();
+	}
+
+	public void ReadPlayerName()
+	{
+		pName = iField.text; 
+		Debug.Log ("Player Name: " + pName);
+	}
+
+	public void GetScore(int score)
+	{
+		//Debug.Log ("Score Passed: "+score);
+		int pScore = score;
+		//Debug.Log ("Score Assigned: " + pScore);
+		PlayerData (pName, pScore);
+	}
+
+	void PlayerData (string playerName,int playerScore)
+	{
+		//Debug.Log ("Player Name: "+playerName); //Displaying the player name in the console
+		Debug.Log ("Player Name: " + playerName + " Score: " + playerScore);
+		nameList.Add (playerName);
+		scoreList.Add (playerScore);
+		scoreList.Sort ();
+		PlayerPrefs.Save ();
+		trackObj.InsertLine (playerName, playerScore);
+	}
+
+
+	
+	
+	
+}
---
+++ TestRep/TrackScore.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+public class TrackScore : MonoBehaviour {
+	
+	string filePath;
+
+	// Use this for initialization
+	void Start () {
+
+	 	filePath = Application.dataPath + Path.DirectorySeparatorChar + "Data" + Path.DirectorySeparatorChar;
+	}
+	
+	public void InsertLine(string name, int score)
+	{
+		using (StreamWriter fileWriter=new StreamWriter(filePath+"TrackGameScore.txt",true)) 
+		{
+			fileWriter.WriteLine("  "+name+"           "+score);
+		}
+	}
+}
